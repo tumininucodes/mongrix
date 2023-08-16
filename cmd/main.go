9b515bce334ebefed613b5071af3049e6059457b
Oluwatumininu Ojo <oluwatumininuojo25@gmail.com>
2023-08-16 04:49:02 +0100

    development

--- cmd/main.go
+++ cmd/main.go
@@ -14,7 +14,7 @@ func main() {
 	
 	// Use the SetServerAPIOptions() method to set the Stable API version to 1
 	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
-	opts := options.Client().ApplyURI("mongodb+srv://oluwatumininuojo25:<Alade2001.>@moroshoi.iflmvgn.mongodb.net/?retryWrites=true&w=majority").SetServerAPIOptions(serverAPI)
+	opts := options.Client().ApplyURI("mongodb+srv://oluwatumininuojo25:@moroshoi.iflmvgn.mongodb.net/?retryWrites=true&w=majority").SetServerAPIOptions(serverAPI)
 	// Create a new client and connect to the server
 	client, err := mongo.Connect(context.TODO(), opts)
 	if err != nil {
