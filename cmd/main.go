e0a630e023c944b2187c4372d5d5cdd3c4f3b813
Oluwatumininu Ojo <oluwatumininuojo25@gmail.com>
2023-08-23 14:15:06 +0100

    development

--- cmd/main.go
+++ cmd/main.go
@@ -30,8 +30,28 @@ func main() {
 	})
 
 	server.POST("add", func(ctx *gin.Context) {
+
 		var inputData map[string]interface{}
-		ctx.ShouldBindJSON(inputData)
+		if err := ctx.ShouldBindJSON(&inputData); err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{"error": "Invalid JSON data"})
+			return
+		}
+
+		bsonData, err := bson.Marshal(inputData)
+		if err != nil {
+			ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to convert JSON to BSON"})
+			return
+		}
+
+
+		println("input data ", inputData)
+		println("bson data ", bsonData)
+		
+		if err := bson.UnmarshalExtJSON([]byte(jsonData), false, &bsonData); err != nil {
+			fmt.Println("Error converting JSON to BSON:", err)
+			return
+		}
+
 		object := bson.M(inputData)
 		println(object)
 		result, err := internal.InsertObject(&object, client, &context)
