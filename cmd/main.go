52d6f2622454e7d7f68b765c03d339aa912bff35
Oluwatumininu Ojo <oluwatumininuojo25@gmail.com>
2023-08-23 15:02:44 +0100

    development

--- cmd/main.go
+++ cmd/main.go
@@ -37,24 +37,14 @@ func main() {
 			return
 		}
 
-		bsonData, err := bson.Marshal(inputData)
-		if err != nil {
-			ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to convert JSON to BSON"})
-			return
-		}
-
-
-		println("input data ", inputData)
-		println("bson data ", bsonData)
+		bsonObject := bson.M(inputData)
 		
-		if err := bson.UnmarshalExtJSON([]byte(jsonData), false, &bsonData); err != nil {
-			fmt.Println("Error converting JSON to BSON:", err)
-			return
-		}
+		// if err := bson.UnmarshalExtJSON([]byte(inputData), false, &bsonData); err != nil {
+		// 	fmt.Println("Error converting JSON to BSON:", err)
+		// 	return
+		// }
 
-		object := bson.M(inputData)
-		println(object)
-		result, err := internal.InsertObject(&object, client, &context)
+		result, err := internal.InsertObject(&bsonObject, client, &context)
 		if err != nil {
 			ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 			return
--- internal/controller.go
+++ internal/controller.go
@@ -81,7 +81,10 @@ func InsertObject(object *bson.M, client *mongo.Client, ctx *context.Context) (b
 		return nil, err
 	}
 
+	println("last insert id: ", result.InsertedID)
+	
 	var inserted bson.M
-	collection.FindOne(*ctx, result.InsertedID).Decode(&inserted)
+	collection.FindOne(*ctx, bson.M{"_id": result.InsertedID}).Decode(&inserted)
+	println("inserted: ", inserted)
 	return inserted, nil
 }
